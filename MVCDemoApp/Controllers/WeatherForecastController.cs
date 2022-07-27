9efbc083b1db74c4b9d6fb060de0394cad07dd3c
GEmma <gemma.walker01@outlook.com>
2022-07-27 10:16:16 +0100

    test

--- MVCDemoApp/Controllers/WeatherForecastController.cs
+++ MVCDemoApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MVCDemoApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "makeChanges"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
